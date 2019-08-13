0b5c9796fa3f64094ed434014f74ce4be34f7c02
henriquefaraco <38503875+henriquefaraco@users.noreply.github.com>
2019-08-13 15:35:23 -0300

    Add files via upload

--- programacao.js
+++ programacao.js
@@ -3,6 +3,9 @@ $(function(){
 	$('#form-familias').hide();
 	$('#form-individuais').hide();
 	$('#form-grupos').hide();
+	$('#btn-cancela').hide();
+	$('#btn-confirma').hide();
+
 
 
 	$('#btn-familias').click(function(e){
@@ -10,20 +13,44 @@ $(function(){
 		$('#form-familias').show();
 		$('#form-individuais').hide();
 		$('#form-grupos').hide();
+		$('#btn-familias').hide();
 		$('#btn-individuais').hide();
 		$('#btn-grupos').hide();
 	});
-	$('#btn-individuais').click(function(e){
 		e.preventDefault();
 		$('#form-familias').hide();
 		$('#form-individuais').show();
 		$('#form-grupos').hide();
+		$('#btn-familias').hide();
+		$('#btn-individuais').hide();
+		$('#btn-grupos').hide();
 	});
 	$('#btn-grupos').click(function(e){
 		e.preventDefault();
 		$('#form-familias').hide();
 		$('#form-individuais').hide();
 		$('#form-grupos').show();
+		$('#btn-familias').hide();
+		$('#btn-individuais').hide();
+		$('#btn-grupos').hide();
+	});
+	$('#btn-cancela').click(function(e){
+		e.preventDefault();
+		$('#form-familias').hide();
+		$('#form-individuais').hide();
+		$('#form-grupos').hide();
+		$('#btn-familias').show();
+		$('#btn-individuais').show();
+		$('#btn-grupos').show();
+	});
+	$('#btn-confirma').click(function(e){
+		e.preventDefault();
+		$('#form-familias').hide();
+		$('#form-individuais').hide();
+		$('#form-grupos').hide();
+		$('#btn-familias').show();
+		$('#btn-individuais').show();
+		$('#btn-grupos').show();
 	});
 
 });
